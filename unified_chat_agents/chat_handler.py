a5f78fb8d3298c8c7b8af432415697c9ec7cfa05
MSR <msujithr@gmail.com>
2023-12-22 13:32:14 +0530

    [CHORE]Changed the ChatMessage Base class

--- unified_chat_agents/chat_handler.py
+++ unified_chat_agents/chat_handler.py
@@ -1,12 +1,12 @@
 import os
 import awsgi
-from flask import (Flask, jsonify, request)
-from utils.log_utils import (logger, LogUtils)
-from lib.chat import ChatRole, ChatRoom
-from models.vector_db.weaviate.weaviate_dao import WeaviateDAO
-from utils.log_utils import (logger, LogUtils)
-from aws_utils.secrets_manager import SecretsManager
 import json
+from flask import (Flask, jsonify, request)
+
+from utils.log_utils import (logger, LogUtils)
+from lib.chat import ChatRole, ChatRoom, ChatMessage
+from models.vector_db.weaviate.weaviate_dao import WeaviateDAO
+from aws_utils.secrets_manager import SecretsManager
 
 secrets = SecretsManager.get_secret("UCA")
 os.environ["OPENAI_API_KEY"] = secrets["OPENAI_KEY"]
@@ -15,17 +15,18 @@ os.environ["WEAVIATE_URL"] = "https://unified-chat-agents-el9cpwl9.weaviate.netw
 
 app = Flask(__name__)
 
+
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = "Hello"
-    chat_messages = [{"from_": ChatRole.USER,
-                      "to": ChatRole.UQRA, "content": user_message}]
+    chat_messages = [ChatMessage(**{"from_": ChatRole.USER,
+                                    "to": ChatRole.UQRA, "content": user_message})]
     chat_room = ChatRoom("test_session_id_1", chat_messages)
     try:
         chat_room.chat()
-        res = chat_room._get_last_message()
+        res = chat_room.get_last_message()
         logger.info(f"Response: {res}")
-        return res["content"]
+        return res.model_dump_json()
     except Exception as e:
         logger.error(f"Error: {e}")
 
@@ -35,8 +36,10 @@ def ingest_api_docs():
     logger.info(f"Request: {request}")
     data = json.loads(request.data)
     weaviate_dao = WeaviateDAO()
-    saved_ids = weaviate_dao.insert_list(doc_info_list=data.get('data'), schema=data.get('class'))
+    saved_ids = weaviate_dao.insert_list(
+        doc_info_list=data.get('data'), schema=data.get('class'))
     return jsonify({'message': 'Success', 'ids': saved_ids})
+
 
 @app.route('/<class_name>/docs', methods=['GET'])
 def fetch_all_docs(class_name: str):
@@ -44,6 +47,7 @@ def fetch_all_docs(class_name: str):
     weaviate_dao = WeaviateDAO()
     docs = weaviate_dao.fetch_all(schema=class_name)
     return jsonify({'message': 'Success', 'docs': docs})
+
 
 @app.route('/<class_name>', methods=['DELETE'])
 def delete_class(class_name: str):
